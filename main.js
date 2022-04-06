13252921230d5a4a06e676a0f9157ea094b0fb8a
Karthikeyan Vignesh <94837687+KarthikVigneshJayabalan@users.noreply.github.com>
2022-04-06 16:26:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+//Create a reference for canvas 
+
+//Give specific height and width to the car image
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+//Set initial position for a car image.
+
+function add() {
+	//upload car, and background images on the canvas.
+}
+
+function uploadBackground() {
+	//Define function ‘uploadBackground’
+
+}
+
+function uploadgreencar() {
+	//Define function ‘uploadgreencar’.
+
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+
+
+function down()
+{
+	
+
+function left()
+{
+	
+}
+
+function right()
+{
+	
+}
